c5e44df31d0927fb64d5ef44b1b7f4d6e7cdf246
Remolachabot <mauriciocazares32@gmail.com>
2023-03-12 18:29:53 -0600

    Curso de Python

--- 23_03_12_001_Variables_Cap2_v1_0.py
+++ 23_03_12_001_Variables_Cap2_v1_0.py
@@ -1,7 +1,7 @@
 # Curso de Python - Cap 2
-message = 'Hola Mundo'
-a = 100
+message = 'Hola Mundo'      # Crea una variable de tipo String
+a = 100                     # Crea una variable de tipo Int
 b = 50
-c = a + b
-print(message)
-print(c)
+c = a + b                   # Suma las dos variables Int
+print(message)              # Imprime la variable "message"
+print(c)                    # Imprime el resultado de la suma
---
+++ 23_03_12_Strings_1_Cap3_v1_0.py
@@ -0,0 +1,8 @@
+# Curso de Python - Cap 3
+message1_str = 'String de comillas simples'                                     # Mensaje usando comillas simples
+message2_str = "String de comillas dobles"                                      # Mensaje usando comillas dobles
+message3_str = '"print()" se utiliza para imprimir valores en la consola.'      # Mensaje usando comillas simples
+                                                                                # Contiene comillas dobles
+print(message1_str)                                                             # Imrpime las variables
+print(message2_str)
+print(message3_str)
